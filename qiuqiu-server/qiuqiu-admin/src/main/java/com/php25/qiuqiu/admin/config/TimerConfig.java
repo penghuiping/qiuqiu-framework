a7045eaeb5c4d8dbc0eea7483ef8b9bca3d002a9
penghuiping <575813261@qq.com>
2021-03-13 18:53:23 +0800

    任务模块

--- qiuqiu-server/qiuqiu-admin/src/main/java/com/php25/qiuqiu/admin/config/TimerConfig.java
+++ qiuqiu-server/qiuqiu-admin/src/main/java/com/php25/qiuqiu/admin/config/TimerConfig.java
@@ -1,18 +1,8 @@
 package com.php25.qiuqiu.admin.config;
 
-import com.php25.common.db.DbType;
-import com.php25.common.mq.MessageQueueManager;
-import com.php25.common.redis.RedisManager;
 import com.php25.common.timer.Timer;
-import com.php25.qiuqiu.job.manager.JobManager;
-import com.php25.qiuqiu.job.manager.RedisJobManager;
-import com.php25.qiuqiu.job.repository.JobModelRepository;
-import com.php25.qiuqiu.job.repository.JobModelRepositoryImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.concurrent.ExecutorService;
 
 /**
  * @author penghuiping
@@ -20,14 +10,6 @@ import java.util.concurrent.ExecutorService;
  */
 @Configuration
 public class TimerConfig {
-
-    @Bean
-    JobManager jobManager(JobModelRepository jobModelRepository
-            , MessageQueueManager messageQueueManager
-            , RedisManager redisManager
-            , ExecutorService pool) {
-        return new RedisJobManager(jobModelRepository, messageQueueManager, redisManager, pool);
-    }
 
     @Bean
     Timer timer() {
